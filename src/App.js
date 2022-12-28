115be51d7fbde626ee695e2d91e9c469110291b3
bhaskarmavuri <bachi.mavuri@gmail.com>
2022-12-28 23:24:19 +0530

    Added Not Found Functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing/Landing';
+import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+      <Router>
+        <div className="App">
+        <Routes>
+          <Route path='/' element={<Landing/>} />  
+          <Route path='*' element={<NotFound/>} />  
+        </Routes>
+        </div>
+      </Router>
   );
 }
 
